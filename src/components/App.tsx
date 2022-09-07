d94928a206b9b5a95e701e847502923452959812
Dima <katri4dimon@i.ua>
2022-09-07 16:00:44 +0300

    component styling and added Header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,18 +4,31 @@ import moment from 'moment';
 import Header from "./Header/Header";
 import TitleWeeks from "./TitleWeeks/TitleWeeks";
 import CalendarGrid from "./CalendarGrid/CalendarGrid";
+import styled from 'styled-components';
+
+const ShadowWrapper = styled('div')`
+  border-top: 1px solid #737374;
+  border-left: 1px solid #464648;
+  border-right: 1px solid #464648;
+  border-bottom: 2px solid #464648;
+  border-radius: 8px;
+  overflow:hidden;
+  box-shadow: 0 0 0 1px #1A1A1A, 0 8px 20px 6px #888;
+`;
+
 
 function App() {
 
     moment.updateLocale('en', {week: {dow: 1}});
-    const startDay = moment().clone().startOf('month').startOf('week');
+    const today = moment();
+    const startDay = today.clone().startOf('month').startOf('week');
 
     return (
-        <div className="App">
-            <Header/>
+        <ShadowWrapper>
+            <Header today={today}/>
             <TitleWeeks/>
             <CalendarGrid startDay={startDay}/>
-        </div>
+        </ShadowWrapper>
     );
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import styled from "styled-components";
+import moment from "moment";
 
-const Header = () => {
+interface PropsHeader {
+    today: moment.Moment
+}
+
+const DivWrapper = styled('div')`
+    display: flex;
+    justify-content: space-between;
+    background-color: #1E1F21;
+	color: #DCDDDD;
+	padding: 16px;
+`;
+
+const TextWrapper = styled('span')`
+  font-size: 32px;
+`;
+
+const TitleWrapper = styled(TextWrapper)`
+  font-weight: bold;
+  margin-right: 8px;
+`;
+
+const ButtonsWrapper = styled('div')`
+  display: flex;
+  align-items: center;
+`;
+
+const ButtonWrapper = styled('button')`
+    border: unset;
+	background-color: #565759;
+	height: 20px;
+	margin-right: 2px;
+	border-radius: 4px;
+	color: #E6E6E6;
+`;
+
+const TodayButton = styled(ButtonWrapper)`
+    padding-right: 16px;
+    padding-left: 16px;
+	font-weight: bold;
+`;
+
+
+const Header = ({today}: PropsHeader) => {
     return (
-        <div>
-            Header
-        </div>
+        <DivWrapper>
+            <div>
+                <TitleWrapper>{today.format('MMM')}</TitleWrapper>
+                <TextWrapper>{today.format('YYYY')}</TextWrapper>
+            </div>
+            <ButtonsWrapper>
+                <ButtonWrapper> &lt; </ButtonWrapper>
+                <TodayButton>Today</TodayButton>
+                <ButtonWrapper> &gt; </ButtonWrapper>
+            </ButtonsWrapper>
+        </DivWrapper>
     );
 };
 
