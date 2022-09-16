763627d81b51d184d827d796436fd7cad18e8374
Dima <katri4dimon@i.ua>
2022-09-16 14:02:09 +0300

    update eventsApi

--- src/api/eventsApi.ts
+++ src/api/eventsApi.ts
@@ -1,8 +1,21 @@
 import {instance} from "./baseApi";
 import {IEvent} from "../types/IEvents";
 
+interface IResponse {
+    resultCode: number
+}
+
 export const eventsApi = {
     getEvents() {
         return instance.get<IEvent[]>('/events').then(res => res.data)
     },
+    addEvent(newEvent: IEvent) {
+        return instance.post<IResponse>('/events/add', {newEvent}).then(res => res.data)
+    },
+    updateEvent(eventId: number, event: IEvent) {
+        return instance.put<IResponse>(`/events/update/${eventId}`, {event}).then(res => res.data)
+    },
+    deleteEvent(eventId:number) {
+        return instance.delete<IResponse>(`/events/delete/${eventId}`).then(res => res.data)
+    }
 }
